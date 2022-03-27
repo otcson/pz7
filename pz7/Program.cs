8da742a14d3342e0c2632b100b84b8e74cdfdc03
otcson <vovaprosto52@gmail.com>
2022-03-27 11:43:43 +0300

    Добавьте файлы проекта.

---
+++ pz7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pz7
+{
+    enum processor {Intel, Amd}
+    enum matplata {Esonic,Intel,Gigabyte,AsRock }
+    enum videokarta {Asus, Gigabyte, Msi, PowerColor}
+    class Computer
+    {
+        public processor proc;
+        public matplata plata;
+        public videokarta vkarta;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer My = new Computer();
+            My.proc = processor.Amd;
+            My.plata = matplata.Gigabyte;
+            My.vkarta = videokarta.PowerColor;
+        }
+    }
+}
